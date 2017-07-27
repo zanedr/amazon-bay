460f06b2de4ad73dc595d2d9f2a6bf56514d8216
Zane Russell <zaneroosa@gmail.com>
2017-07-27 17:35:04 -0600

    prices accumulating and posting to page

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 window.onload = retrieveInformation();
+let totalPrice = 0
 
 function retrieveInformation(){
     retrieveCart();
@@ -31,10 +32,18 @@ function retrieveItems(){
 
 function retrieveHistory(){
     console.log('retrieve history')
-//   for (var key in localStorage){
-//     var tempObject = JSON.parse(localStorage[key])
-//     createCard(tempObject);
-//   }
+    fetch("/api/v1/purchasehistory").then( res => {
+    res.json()
+      .then(info => {
+          if(info.length) {
+              info.forEach(item => {
+             // createHistoryCard(item)
+             console.log(item)
+              })
+          }
+        console.log(info)
+      });
+    });
 }
 
 function createItemCards(info) {
@@ -42,13 +51,49 @@ function createItemCards(info) {
         $('#card-container').append(`
             <div class='item-card'>
                 <h4 class='item-title'>${item.title}</h4>
-                <p>Description</p>
+                <p class='item-description-title'>Description</p>
                 <h5 class='item-description'>${item.description}</h5>
                 <img class='item-picture' src='${item.picture}'>
                 <h6 class='item-price'>$${correctPrice(item.price)}</h6>
+                <button class='select-item'>Add to cart</button>
             </div>`)
     })
 }
+
+$('#card-container').on('click', '.select-item', function(){
+    var itemTitle = $(this).siblings('.item-title').text()
+    var itemDescription = $(this).siblings('.item-description').text()
+    var itemPrice = $(this).siblings('.item-price').text()
+    console.log(itemTitle, itemDescription, itemPrice)
+    addToCart(itemTitle, itemDescription, itemPrice)
+})
+
+function addToCart(itemTitle, itemDescription, itemPrice) {
+    $('#shopping-cart-information-container').append(`
+        <div class='shopping-cart-item'>
+            <h3>${itemTitle} ${itemPrice}</h3>
+        </div>
+    `)
+    changeCartTotal(itemPrice)
+}
+
+function changeCartTotal(itemPrice) {
+    let newItemPrice = itemPrice.slice(1)
+    // newItemPrice.split('').shift().join('')
+    console.log('changetotal', newItemPrice)
+    totalPrice = totalPrice + parseFloat(newItemPrice)
+    $('#shopping-cart-total').text('Total: $' +totalPrice + '.00')
+}
+
+$('#purchase-all').on('click', function(){
+    fetch('/api/v1/addpurchasehistory', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({"price": totalPrice})
+    }) .then(() => {
+        
+    })
+})
 
 function correctPrice(price) {
     return (price / 100).toFixed(2)
