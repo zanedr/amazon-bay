584efd3260c391e16ea78d946b78bc5e40438e4a
Zane Russell <zaneroosa@gmail.com>
2017-07-27 13:23:46 -0600

    updated react syntax

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,21 @@
-const React = require('react')
+var React = require('react');
 
+module.exports = class Main extends React.Component{
 
-module.exports = React.createClass({
     render() {
         return (
-            <div>
-                <h1>here goes nuthin'</h1>
-            </div>
-        )
+            <html>
+                <head>
+                    <title>Universal App with React</title>
+                    <link rel='stylesheet' href='/style.css' />
+                </head>
+                <body>
+                    <div>
+                        <h1>YEAH!</h1>
+                        <button onClick={this._handleClick}>Click Me</button>
+                    </div>
+                </body>
+            </html>
+        );
     }
-})
+};
