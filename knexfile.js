8f03824ac53aaeeaa4097a1accc5e24f58480ffb
Zane Russell <zaneroosa@gmail.com>
2017-07-27 13:50:24 -0600

    get routes for items working

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,14 @@
 module.exports = {
 
+   production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/ambay',
@@ -10,6 +19,18 @@ module.exports = {
      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/ambay',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
   }
 
 
--- server.js
+++ server.js
@@ -1,15 +1,67 @@
-require('babel-register')({
-    presets: ['react', 'es2015']
-});
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 var express = require('express');
 var app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', function(request, response) {
-    response.sendFile('index.html');
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
 });
+
+//GET route for items
+app.get('/items', (req, res) => {
+  database('items').select()
+  .then((items) => {
+    if (!items) {
+      return res.status(404).send({
+        error: 'No items were found',
+      });
+    } else {
+      return res.status(200).json(items);
+    }
+  }).catch((error) => {
+    return res.status(500);
+  });
+});
+
+//GET route for purchase history
+app.get('/purchasehistory', (req, res) => {
+  database('purchasehistory').select()
+  .then((purchasehistory) => {
+    if (!purchasehistory.length) {
+      return res.status(404).send({
+        error: 'No order history',
+      });
+    } else {
+      return res.status(200).json(purchasehistory);
+    }
+  }).catch((error) => {
+    return res.status(500);
+  });
+});
+
+//POST route for purchase history
+app.post('/addpurchasehistory', (req, res) => {
+    console.log('body', req.body)
+//   const { price } = req.body
+
+  if (!price) {
+    return response.status(422).send({
+      error: 'No price included with purchase history'
+    })
+  }
+//     else {
+//       database('purchashistory').insert({ price }, 'id')
+//       .then(() => {
+//         return res.status(201)
+//       }).catch((error) => {
+//         return res.status(500);
+//     });
+//     }
+  });
 
 var PORT = 3000;
 app.listen(PORT, function() {
