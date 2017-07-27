c3e9d075b49721bcc17bad48a0ac81422418ee3b
Zane Russell <zaneroosa@gmail.com>
2017-07-27 13:26:40 -0600

    more syntax changes

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,14 +6,11 @@ module.exports = class Main extends React.Component{
         return (
             <html>
                 <head>
-                    <title>Universal App with React</title>
+                    <title>Reaction</title>
                     <link rel='stylesheet' href='/style.css' />
                 </head>
                 <body>
-                    <div>
-                        <h1>YEAH!</h1>
-                        <button onClick={this._handleClick}>Click Me</button>
-                    </div>
+                    
                 </body>
             </html>
         );
