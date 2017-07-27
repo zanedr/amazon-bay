c4e4bbb3e4fd5191df06bab5793a9ef4b74755bd
Zane Russell <zaneroosa@gmail.com>
2017-07-27 13:30:43 -0600

    ditching react. going with jquery

--- server.js
+++ server.js
@@ -4,17 +4,11 @@ require('babel-register')({
 
 var express = require('express');
 var app = express();
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var Main = require('./components/Main.jsx')
 
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', function(request, response) {
-    var html = ReactDOMServer.renderToString(
-        React.createElement(Main)
-    );
-    response.send(html);
+    response.sendFile('index.html');
 });
 
 var PORT = 3000;
